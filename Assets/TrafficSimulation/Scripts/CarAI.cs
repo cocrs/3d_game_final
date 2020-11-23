6df9e252d9d3262c603e21cd863eedf0cb2ee5b8
Zovjsra <58198508+Zovjsra@users.noreply.github.com>
2020-11-23 16:57:47 +0900

    加入npc車輛並加入一些動作

--- Assets/TrafficSimulation/Scripts/CarAI.cs
+++ Assets/TrafficSimulation/Scripts/CarAI.cs
@@ -7,8 +7,7 @@ using UnityEngine;
 using UnityEngine.AI;
 
 namespace TrafficSimulation {
-    public class CarAI : MonoBehaviour
-    {
+    public class CarAI : MonoBehaviour {
         [Header("Traffic System")]
         public TrafficSystem trafficSystem;
         public int waypointThresh = 6;
@@ -34,8 +33,7 @@ namespace TrafficSimulation {
         float initialTopSpeed;
 
 
-        void Start()
-        {
+        void Start() {
             carController = this.GetComponent<VehiclePhysics>();
 
             initialTopSpeed = Random.Range(minTopSpeed, maxTopSpeed);
@@ -49,20 +47,20 @@ namespace TrafficSimulation {
             agent.height = 1;
             agent.speed = 1;
 
-            if(trafficSystem == null)
+            if (trafficSystem == null)
                 return;
 
             //Find segment
-            foreach(Segment segment in trafficSystem.segments){
-                if(segment.IsOnSegment(this.transform.position)){
+            foreach (Segment segment in trafficSystem.segments) {
+                if (segment.IsOnSegment(this.transform.position)) {
                     curSeg = segment.id;
                     //Find nearest waypoint to start within the segment
                     float minDist = float.MaxValue;
-                    for(int j=0; j<trafficSystem.segments[curSeg].waypoints.Count; j++){
+                    for (int j = 0; j < trafficSystem.segments[curSeg].waypoints.Count; j++) {
                         float d = Vector3.Distance(this.transform.position, trafficSystem.segments[curSeg].waypoints[j].transform.position);
                         //Only take in front points
                         Vector3 lSpace = this.transform.InverseTransformPoint(trafficSystem.segments[curSeg].waypoints[j].transform.position);
-                        if(d < minDist && lSpace.z > 0){
+                        if (d < minDist && lSpace.z > 0) {
                             minDist = d;
                             curWp = j;
                         }
@@ -72,8 +70,8 @@ namespace TrafficSimulation {
             }
         }
 
-        void Update(){
-            if(trafficSystem == null)
+        void Update() {
+            if (trafficSystem == null)
                 return;
 
             //Set navmesh agent in front of the car
@@ -85,7 +83,7 @@ namespace TrafficSimulation {
         }
 
 
-        void WaypointChecker(){
+        void WaypointChecker() {
             GameObject waypoint = trafficSystem.segments[curSeg].waypoints[curWp].gameObject;
             //Position of next waypoint relative to the car
             Vector3 nextWp = this.transform.InverseTransformPoint(new Vector3(waypoint.transform.position.x, this.transform.position.y, waypoint.transform.position.z));
@@ -94,9 +92,9 @@ namespace TrafficSimulation {
             agent.SetDestination(waypoint.transform.position);
 
             //Go to next waypoint if arrived to current
-            if(nextWp.magnitude < waypointThresh){
+            if (nextWp.magnitude < waypointThresh) {
                 curWp++;
-                if(curWp >= trafficSystem.segments[curSeg].waypoints.Count){
+                if (curWp >= trafficSystem.segments[curSeg].waypoints.Count) {
                     curSeg = GetNextSegmentId();
                     curWp = 0;
                 }
@@ -106,63 +104,68 @@ namespace TrafficSimulation {
         /// If vehicle within raycast hit length, return its speed. Otherwise return the original initial top speed of the vehicle.
         /// In order to avoid that the current car is going fast than the front one and collide.
         /// If the car has to stop, return 0
-        float GetCarSpeed(){
+        float GetCarSpeed() {
             //If car has to stop, set speed to 0
-            if(hasToStop)
+            if (hasToStop)
                 return 0;
 
             Vector3 anchor = new Vector3(this.transform.position.x, this.transform.position.y + 1f, this.transform.position.z);
-            if(raycastAnchor != null)
+            if (raycastAnchor != null)
                 anchor = raycastAnchor.position;
-            
+
             //Check if we are going to collide with a car in front
             CarAI otherCarAI = null;
+            GameObject hitObj = null;
             float topSpeed = initialTopSpeed;
             float initRay = (raysNumber / 2f) * raySpacing;
 
-            for(float a=-initRay; a<=initRay; a+=raySpacing){
+            for (float a = -initRay; a <= initRay; a += raySpacing) {
                 float hitDist;
-                CastRay(anchor, a, this.transform.forward, raycastLength, out otherCarAI, out hitDist);
+                CastRay(anchor, a, this.transform.forward, raycastLength, out otherCarAI, out hitDist, out hitObj);
 
                 //If rays collide with a car, adapt the top speed to be the same as the one of the front vehicle
-                if(otherCarAI != null && otherCarAI.carController != null && carController.Topspeed > otherCarAI.carController.Topspeed){
+                if (otherCarAI != null && otherCarAI.carController != null && carController.Topspeed > otherCarAI.carController.Topspeed) {
                     //Check if the car is on the same lane or not. If not the same lane, then we do not adapt the vehicle speed to the one in front
                     //(it just means that the rays are hitting a car on the opposite lane...which shouldn't influence the car's speed)
-                    if(hasToGo && !IsOnSameLane(otherCarAI.transform))
+                    if (hasToGo && !IsOnSameLane(otherCarAI.transform))
                         return topSpeed;
 
                     //If the hit distance is too close, "emergency slow down" the car so they don't collide
-                    else if(hitDist < 2f)
+                    else if (hitDist < 2f)
                         return topSpeed = 0f;
-                    
+
                     //Otherwise adapt the car speed to the one in front
                     topSpeed = otherCarAI.carController.Topspeed - 0.05f;
                     break;
                 }
+                if (hitObj != null) {
+                    return 0f;
+                }
             }
-            
+
             //If no collision detected then keep the car top speed
             return topSpeed;
-        }   
+        }
 
 
-        void MoveVehicle(float _topSpeed){
+        void MoveVehicle(float _topSpeed) {
             //Wheel steering value
             float steering = Mathf.Clamp(this.transform.InverseTransformDirection(agent.desiredVelocity).x, -1f, 1f);
-            
+
             //If car is turning then decrease it's maximum
             float topSpeed = _topSpeed;
-            if(steering > 0.2f || steering < -0.2f && carController.Topspeed > 15) topSpeed = initialTopSpeed / 2f;
+            if (steering > 0.2f || steering < -0.2f && carController.Topspeed > 15) topSpeed = initialTopSpeed / 2f;
             carController.Topspeed = topSpeed;
 
             //Move the car
             carController.Move(steering, 1f, 0f);
         }
 
-        
-        void CastRay(Vector3 anchor, float angle, Vector3 dir, float length, out CarAI outCarAI, out float outHitDistance){
+
+        void CastRay(Vector3 anchor, float angle, Vector3 dir, float length, out CarAI outCarAI, out float outHitDistance, out GameObject outHitObject) {
 
             outCarAI = null;
+            outHitObject = null;
             outHitDistance = -1f;
 
             //Draw raycast
@@ -172,22 +175,28 @@ namespace TrafficSimulation {
             int layerAuto = 1 << LayerMask.NameToLayer("AutonomousVehicle");
             int layerPlayer = 1 << LayerMask.NameToLayer("Player");
             RaycastHit hit;
-            if(Physics.Raycast(anchor, Quaternion.Euler(0, angle, 0) * dir, out hit, length, layerAuto | layerPlayer)){
+            if (Physics.Raycast(anchor, Quaternion.Euler(0, angle, 0) * dir, out hit, length, layerAuto | layerPlayer)) {
                 outCarAI = hit.collider.GetComponentInParent<CarAI>();
                 outHitDistance = hit.distance;
             }
+            if (Physics.Raycast(anchor, Quaternion.Euler(0, angle, 0) * dir, out hit, length, layerAuto | layerPlayer)) {
+                if (hit.collider.gameObject.tag == "Player") {
+                    outHitObject = hit.collider.gameObject;
+                    outHitDistance = hit.distance;
+                }
+            }
         }
 
-        int GetNextSegmentId(){
-            if(trafficSystem.segments[curSeg].nextSegments.Count == 0)
+        int GetNextSegmentId() {
+            if (trafficSystem.segments[curSeg].nextSegments.Count == 0)
                 return 0;
             int c = Random.Range(0, trafficSystem.segments[curSeg].nextSegments.Count);
             return trafficSystem.segments[curSeg].nextSegments[c].id;
         }
 
-        bool IsOnSameLane(Transform otherCar){
+        bool IsOnSameLane(Transform otherCar) {
             Vector3 diff = this.transform.forward - otherCar.transform.forward;
-            if(Mathf.Abs(diff.x) < 0.3f && Mathf.Abs(diff.z) < 0.3f) return true;
+            if (Mathf.Abs(diff.x) < 0.3f && Mathf.Abs(diff.z) < 0.3f) return true;
             else return false;
         }
     }
