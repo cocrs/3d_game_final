8104137b451108fac4217e759c446fcab6c2eeb0
Zovjsra <4703michael@gmail.com>
2020-12-10 20:38:36 +0800

    下載一堆套件

---
+++ Assets/my/Scripts/generateMap.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generateMap : MonoBehaviour {
+    //Public var
+    public GameObject road;
+    public GameObject grass;
+    public GameObject sidewalk;
+
+    //Local var
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
