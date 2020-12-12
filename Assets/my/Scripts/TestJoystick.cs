08a9bc779814c511e92bd5ae1f56bcc5303c55b2
Zovjsra <4703michael@gmail.com>
2020-12-12 14:17:46 +0800

    改車

---
+++ Assets/my/Scripts/TestJoystick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Input;
+
+public class TestJoystick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
