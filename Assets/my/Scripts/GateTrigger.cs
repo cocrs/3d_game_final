996e1e50b8bd12f5e5c37e90496def5264ebb53e
cocrs <cocrs@users.noreply.github.com>
2020-12-20 16:54:02 +0800

    test

---
+++ Assets/my/Scripts/GateTrigger.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using Fungus;
+
+public class GateTrigger : MonoBehaviour
+{
+    public GameManager gameManager;
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (gameManager.playerDollars >= 10000)
+            {
+                animator.SetInteger("state", 1);
+                Flowchart.BroadcastFungusMessage("playerWithEnoughMoney");
+            }
+            else
+            {
+                Flowchart.BroadcastFungusMessage("playerWithNoEnoughMoney");
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player" && animator.GetInteger("state") == 1)
+        {
+            animator.SetInteger("state", 2);
+
+        }
+    }
+}
+
